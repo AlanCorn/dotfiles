17ff333a9c9a1585749d7b17f8ab9d918ed4c531
AlanCorn_02 <1561206831@qq.com>
2022-11-01 15:56:33 +0800

    add abbr and alias to config.fish;removed tmpfile

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,30 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set theme_color_scheme nord
 
 function fish_greeting
     # toilet AlanCorn --rainbow -f mono9
     # neofetch --iterm2 ~/Pictures/neofetch/
-    neofetch
+    # neofetch
 end
+
+
+# 常用命令别名
+abbr -a yain paru -S
+abbr -a yarem paru -Rns
+
+abbr -a pacin sudo pacman -S
+abbr -a pacloc pacman -Qi
+abbr -a pacrem sudo pacman -Rns
+abbr -a pacreps pacman -Ss
+abbr -a pacupg sudo pacman -Syu
+abbr -a manpp tldr
+abbr -a ga git add
+abbr -a gc git commit
+
+abbr -a aria2c_start aria2c --conf-path=$HOME/.config/aria2/aria2.conf -D
+
+# 配置环境变量
+set -x PATH $HOME/.yarn/bin $PATH
 
 alias ll="exa --long --git"
