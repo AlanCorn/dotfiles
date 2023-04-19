dcf79be408e766af7a64ec30bc0f430cbf06c76c
AlanCorn <1561206831@qq.com>
2023-04-19 16:39:13 +0800

    kitty下快捷键tips

---
+++ fish/functions/tmux_tips.fish
@@ -0,0 +1,8 @@
+function tmux_tips 
+	echo -e "\033[36mhttps://github.com/ttys3/my-kitty-config\033[0m"
+	echo -e "\033[36mCreate new tab:\033[0m\033[35mctrl+a>c\033[0m"
+	echo -e "\033[36mclose window/tab:\033[0m\033[35mctrl+a>x\033[0m"
+	echo -e "\033[36mGoto previus tab:\033[0m\033[35mctrl+shift+←\033[0m"
+	echo -e "\033[36mGoto next tab:\033[0m\033[35mctrl+shift+→\033[0m"
+	echo -e "\033[36mChange tab title:\033[0m\033[35mctrl+a>,\033[0m"
+end
