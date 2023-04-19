65d1669ed832a18a08ff686fa69ebdfb12faf37b
AlanCorn_02 <1561206831@qq.com>
2023-04-19 15:59:34 +0800

    neovim以及其插件配置

---
+++ nvim/init.lua
@@ -0,0 +1,16 @@
+require("core.options")
+require("core.keymaps")
+
+require("plugins.plugins-setup")
+
+-- 插件
+require("plugins.lualine")
+require("plugins/nvim-tree")
+require("plugins/treesitter")
+require("plugins/lsp")
+require("plugins/cmp")
+require("plugins/comment")
+require("plugins/autopairs")
+require("plugins/bufferline")
+require("plugins/gitsigns")
+require("plugins/telescope")
